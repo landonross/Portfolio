dfa717adc67e8465a4fd2b35aae2b65b7f52235a
Landon Ross <landon.w.ross@gmail.com>
2021-08-18 18:41:34 -0600

    fixing heroku

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const express = require("express");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path')
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
